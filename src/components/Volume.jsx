c88fcfc0b04ac8d2aabac765773864dd5a2dd63c
cypl <cyrille.plenchette@me.com>
2024-03-06 07:09:15 +0100

    add a custom visualisation

--- src/components/Volume.jsx
+++ src/components/Volume.jsx
@@ -7,7 +7,14 @@ function Volume({ volume, setVolume }){
             <Mute onClick={() => setVolume(0)} className={volume === 0 && "muted"}>
                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.72361 1.05279C7.893 1.13749 8 1.31062 8 1.5V13.5C8 13.6894 7.893 13.8625 7.72361 13.9472C7.55421 14.0319 7.35151 14.0136 7.2 13.9L3.33333 11H1.5C0.671573 11 0 10.3284 0 9.5V5.5C0 4.67158 0.671573 4 1.5 4H3.33333L7.2 1.1C7.35151 0.986371 7.55421 0.968093 7.72361 1.05279ZM7 2.5L3.8 4.9C3.71345 4.96491 3.60819 5 3.5 5H1.5C1.22386 5 1 5.22386 1 5.5V9.5C1 9.77614 1.22386 10 1.5 10H3.5C3.60819 10 3.71345 10.0351 3.8 10.1L7 12.5V2.5ZM14.8536 5.14645C15.0488 5.34171 15.0488 5.65829 14.8536 5.85355L13.2071 7.5L14.8536 9.14645C15.0488 9.34171 15.0488 9.65829 14.8536 9.85355C14.6583 10.0488 14.3417 10.0488 14.1464 9.85355L12.5 8.20711L10.8536 9.85355C10.6583 10.0488 10.3417 10.0488 10.1464 9.85355C9.95118 9.65829 9.95118 9.34171 10.1464 9.14645L11.7929 7.5L10.1464 5.85355C9.95118 5.65829 9.95118 5.34171 10.1464 5.14645C10.3417 4.95118 10.6583 4.95118 10.8536 5.14645L12.5 6.79289L14.1464 5.14645C14.3417 4.95118 14.6583 4.95118 14.8536 5.14645Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
             </Mute>
-            <input type="range" min="0" max="1" step="0.01" value={volume} onChange={(event) => setVolume(Number(event.target.value))}></input>
+            <Range>
+                <input type="range" min="0" max="1" step="0.01" value={volume} onChange={(event) => setVolume(Number(event.target.value))}></input>
+                <span className="bar-volume">
+                    <span className="bar-volume--bg">
+                        <span className="bar-volume--data" style={{width: volume*100 + "%"}}></span>
+                    </span>
+                </span>
+            </Range>
             <Indicator>
                 {
                     volume > 0.8 &&
@@ -37,12 +44,6 @@ const Wrapper = styled.div`
     width:14rem;
     margin:2rem auto;
     display:flex;
-    & input{
-        width:calc(100% - 3rem);
-        margin:auto;
-        display:block;
-        height:1rem;
-    }
 `
 const Mute = styled.div`
     height:1rem;
@@ -76,6 +77,44 @@ const Indicator = styled.div`
         height:1rem;
         &.muted{
             opacity:0.3;
+        }
+    }
+`
+const Range = styled.span`
+    position:relative;
+    width:calc(14rem - 2rem);
+    & input{
+        width:calc(100% - 1rem);
+        margin:auto;
+        display:block;
+        height:1rem;
+        position:relative;
+        z-index:1;
+        opacity:0;
+    }
+    & .bar-volume{
+        position:absolute;
+        height:1rem;
+        width:100%;
+        top:0;
+        left:0;
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        & .bar-volume--bg{
+            position:absolute;
+            height:0.4rem;
+            width:calc(100% - 1rem);
+            border-radius:0.2rem;
+            background-color:rgba(255,255,255,0.2);
+            overflow:hidden;
+            & .bar-volume--data{
+                position:absolute;
+                height:0.4rem;
+                left:0;
+                top:0;
+                background-color:rgba(255,255,255,1);
+            }
         }
     }
 `
