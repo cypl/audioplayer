d7f0f9c0ab949416fe7eefc3b8f14c39ca6fb4c9
cypl <cyrille.plenchette@me.com>
2024-03-06 07:06:53 +0100

    UI colors and sizes

---
+++ src/utils/UI.js
@@ -0,0 +1,13 @@
+export const colorsUI = {
+    background: "rgba(0,0,0,0.65)",
+    border:"rgba(255,255,255,0.1)",
+    textInactive:"rgba(255,255,255,0.6)",
+    textActive:"rgba(255,255,255,1)",
+}
+
+export const sizesUI = {
+    radius: "0.35rem",
+    radiusBig: "0.45rem",
+    text: "0.85rem",
+    textSmall: "0.65rem",
+}
