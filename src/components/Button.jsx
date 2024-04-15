4dff7ab51396cd1309c20e1531612740e8c0ac68
cypl <cyrille.plenchette@me.com>
2024-04-15 22:05:07 +0200

    remove inactive prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { colorsUI } from '../utils/UI'
 
-function Button({action, inactive, icon}){
+function Button({action, centered, icon}){
+    
     return(
-        <Wrapper onClick={action} className={inactive ? "inactive" : ""}>
+        <Wrapper onClick={action} className={centered ? "centered" : ""}>
             {icon}
         </Wrapper>
     )
@@ -13,24 +15,31 @@ export default Button
 
 Button.propTypes = {
     action: PropTypes.func,
-    inactive: PropTypes.bool,
+    centered: PropTypes.bool,
     icon: PropTypes.element,
 }
 
 const Wrapper = styled.button`
-    border-radius: 8px;
+    border-radius: 0;
     border: 1px solid transparent;
     padding: 0.6em 1.2em;
     font-size: 1em;
     line-height:1;
     font-weight: 500;
     font-family: inherit;
-    background-color: #1a1a1a;
+    background:${colorsUI.background};
     cursor: pointer;
     transition: border-color 0.25s;
-    margin:0 0.5rem;
+    margin:0;
+    height:2.2rem;
+    width:3.4rem;
+    display:inline-block;
     &.inactive{
         opacity:0.5;
         pointer-events: none;
     }
+    &.centered{
+        border-left:1px solid ${colorsUI.border};
+        border-right:1px solid ${colorsUI.border};
+      }
 `
