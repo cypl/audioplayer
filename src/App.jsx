2de112e104b4bc7c1ad96447f2ddb5bc91a45fdd
Cyrille Plenchette <cyrille.plenchette@me.com>
2024-06-21 06:10:00 +0200

    add Konva visualizer

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ import IconPlay from './components/IconPlay';
 import IconPause from './components/IconPause';
 import AudioVisualizer from './components/AudioVisualizer';
 import AudioVisualizerParticles from './components/AudioVisualizerParticules';
-import AudioVisualizerParticles2 from './components/AudioVisualizerParticules2';
+//import AudioVisualizerParticles2 from './components/AudioVisualizerParticules2';
+import AudioVisualizerKonva from './components/AudioVisualizerKonva';
 
 
 // Déclaration du contexte audio à l'extérieur du composant pour qu'il soit partagé globalement
@@ -282,7 +283,7 @@ useEffect(() => {
         <AudioVisualizerParticles dataFrequencyLeft={dataFrequencyLeft} dataFrequencyRight={dataFrequencyRight} showGrid={grid}/>
       }
       {selectedVisualizer === "third" && 
-        <AudioVisualizerParticles2 dataFrequencyLeft={dataFrequencyLeft} dataFrequencyRight={dataFrequencyRight} showGrid={grid}/>
+        <AudioVisualizerKonva dataFrequencyLeft={dataFrequencyLeft} dataFrequencyRight={dataFrequencyRight} showGrid={grid}/>
       }
       <GridTrigger onClick={() => setGrid(!grid)}></GridTrigger>
     </>
